07186893a364d1a8918f5af888b9829eac01c16a
Alessio Coser <alessio.coser@xpeppers.com>
2020-01-29 11:41:31 +0100

    fixes auto-deploy script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,8 @@ PROJECT_DIR=$(cd $(dirname $0)/..; pwd -P)
 PROJECT_NAME=$(basename $PROJECT_DIR)
 TAG=$1
 
+cd ${PROJECT_DIR}
+
 sed -ie "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" build.gradle
 sed -ie "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" README.md
 sed -ie "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
