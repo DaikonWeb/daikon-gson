0a6c804c6d1e8e6c7316488a8a2b7453d122e3d6
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-01 09:47:32 +0200

    updated publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,9 +10,9 @@ if git rev-parse "$TAG" >/dev/null 2>&1; then
   exit 0
 fi
 
-sed -i -e "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" build.gradle
-sed -i -e "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" README.md
-sed -i -e "s|<version>.*</version>|<version>${TAG}</version>|g" README.md
+sed -i -e "s|'com.github.DaikonWeb:daikon:.*'|'com.github.DaikonWeb:daikon:${TAG}'|g" build.gradle && rm build.gradle-e
+sed -i -e "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" README.md && rm README.md-e
+sed -i -e "s|<version>.*</version>|<version>${TAG}</version>|g" README.md && rm README.md-e
 
 git commit -am "Release ${TAG}"
 git tag $TAG
